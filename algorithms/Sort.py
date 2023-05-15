99857972ebf4711575a191a3cd2f17718eaf2d4d
Francis Mutiso <franckmutiso@gmail.com>
2023-05-15 19:14:39 +0300

    add bubble sort algorithm

--- algorithms/Sort.py
+++ algorithms/Sort.py
@@ -2,7 +2,7 @@ class Sort:
     def __init__(self) -> None:
         pass
 
-    def insertion_sort(lst):
+    def insertion_sort(self, lst):
         # Using a for loop, iterate through the list
         for i in range(1, len(lst)):
             # Store the current item to be inserted at the right place
@@ -23,5 +23,22 @@ class Sort:
                     break
             # insert the item in the correct index in the array
             lst[index] = item
+
+        return lst
+
+    def bubble_sort(self, lst):
+        # Using a for loop, iterate through the list
+        # An inner array iterates through the list and compares two adjacent items
+        # If the left item is greater than right item, swap them
+        # By the time the inner loop is complete the largest item in the array should be to the right
+        # To avoid redudancy, the end index of the inner array keeps getting smaller as right most items are sorted
+        # Repeat until the whole list is sorted
+
+        for i in range(len(lst)):
+            for j in range(0, len(lst) - i - 1):
+                if lst[j] > lst[j + 1]:
+                    temp = lst[j]
+                    lst[j] = lst[j + 1]
+                    lst[j + 1] = temp
 
         return lst
